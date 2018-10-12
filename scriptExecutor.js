79630c3212e91b48c60f9b2bba8eed936102074e
Luke Hoel <32398032+LukeHoel@users.noreply.github.com>
2018-10-12 11:33:13 -0400

    added arrays
    use arrayDef to define
    use arrayGet/arraySet to get and set

--- scriptExecutor.js
+++ scriptExecutor.js
@@ -39,6 +39,8 @@ var evaluateNode = function(node, method, program){
           var func = getFunctionObject(child, method, program);
           switch(func.type){
             case ("scan"):
+            case("arrayDef"):
+            case("arrayValue"):
               ret = func.content;
             break;
             default:
@@ -106,7 +108,7 @@ var getFunctionObject = function(node, method, program){
     });
     if(!ret){throw new Error("Method with name " + node.name + " not found, or an error occured in evaluation")}
   }
-  return JSON.parse(JSON.stringify(ret));
+  return ret;
 }
 
 var getVariableValue = function(varName, node, method){
@@ -121,7 +123,8 @@ var reservedFunctions = function(node, method, program){
   var ret;
   switch(node.name){
     case("print"):
-      if(node.children.length < 1 || node.children.length > 2){
+      var len = node.children.length;
+      if(len < 1 || len > 2){
         paramsError(node.name);
       }
       ret = evaluateNode({children:[node.children[0]]}, method, program) || " ";//dont return null, return space
@@ -133,6 +136,35 @@ var reservedFunctions = function(node, method, program){
     break;
     case("scan"):
       ret = {type: "scan", content: getUserInput(evaluateNode(node, method, program))};
+    break;
+    case("arrayDef"):
+      var arr = [];
+      node.children.forEach(function(child){
+        //evaluate each child node (parameter), and add them to the new array
+        arr.push(evaluateNode({children:[child]}, method, program));
+      });
+      ret = {type: "arrayDef", content: arr}
+    break;
+    case("arrayGet")://reuse same code for most of get/set code, only changing what is done with it at the end
+    case("arraySet"):
+      //takes two params, array and index
+      var len = node.children.length;
+      if(len != (node.name == "arrayGet" ? 2 : 3)){
+        paramsError(node.name);
+      }
+      var arr = evaluateNode({children:[node.children[0]]}, method, program);
+      if(!Array.isArray(arr)){
+        throw new Error("Wrong type of value in parameter 0 of valueAtIndex method. Must be array");
+      }
+      var index = evaluateNode({children:[node.children[1]]}, method, program);
+      if(index < 0 || index > arr.length){
+        throw new Error("Array index out of range of array");
+      }
+      if(node.name == "arraySet"){
+        arr[index] = evaluateNode({children:[node.children[2]]}, method, program);
+      }
+      ret = {type: "arrayValue", content: arr[index]}//return value regardless
+
     break;
   }
   return ret;
